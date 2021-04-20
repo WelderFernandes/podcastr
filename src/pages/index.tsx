7cb7df18a8321ceb8917d6dfb86bf9580229ddec
Welder n. Fernandes <Welderx3@gmail.com>
2021-04-20 13:30:26 -0300

    feat: estilizando o player

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <h1>Teste</h1>;
+import { useEffect } from "react"
+
+export default function Home(props) {
+
+  console.log(props.episodes);
+  
+
+  return (
+    <>
+    <h1>Index</h1>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+
+  const url = await fetch('http://localhost:3333/episodes')
+  const data = await url.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
